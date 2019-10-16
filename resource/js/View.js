3596b6f1e10b1a3c721734c08b4ad5250a08c16f
George Denning <george1denning@gmail.com>
2019-10-16 22:47:50 +0100

    Added Button Component

--- resource/js/View.js
+++ resource/js/View.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 export default class View {
     constructor(element) {
+        this.base = this.base();
         this.container = element;
         if (!this.container.dataset.view) {
             return false;
@@ -9,9 +10,12 @@ export default class View {
         this.section = this.container.dataset.view;
         this.setState('waiting');
     }
+    base() {
+        return window.location.href + '/resource/view/';
+    }
     render() {
         return new Promise(resolve => {
-            $(this.container).load('/resource/view/' + this.section + '.html', response => {
+            $(this.container).load(this.base + this.section + '.html', response => {
                 this.setState('loaded');
                 resolve(response);
             });
